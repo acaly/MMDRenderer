447222a083c00038f420b51d9e8b02256591cc99
Zhenwei Wu <wuzhenwei35@gmail.com>
2018-05-21 15:49:24 -0400

    Modified Tga decoder.

--- MMDRenderer/TgaDecoder.cs
+++ MMDRenderer/TgaDecoder.cs
@@ -1,129 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MMDRenderer
 {
-    public class TgaDecoder
+    class TgaDecoder
     {
-        protected class TgaData
+        [StructLayout(LayoutKind.Explicit)]
+        private struct TgaHeader
         {
-            private const int TgaHeaderSize = 18;
-            private int idFieldLength;
-            private int colorMapType;
-            private int imageType;
-            private int colorMapIndex;
-            private int colorMapLength;
-            private int colorMapDepth;
-            private int imageOriginX;
-            private int imageOriginY;
-            private int imageWidth;
-            private int imageHeight;
-            private int bitPerPixel;
-            private int descriptor;
-            private byte[] colorData;
+            [FieldOffset(0)] public byte IdFieldLength;
+            [FieldOffset(1)] public byte ColorMapType;
+            [FieldOffset(2)] public byte ImageType;
+            [FieldOffset(3)] public ushort ColorMapIndex;
+            [FieldOffset(5)] public ushort ColorMapLength;
+            [FieldOffset(7)] public byte ColorMapDepth;
+            [FieldOffset(8)] public ushort ImageOriginX;
+            [FieldOffset(10)] public ushort ImageOriginY;
+            [FieldOffset(12)] public ushort ImageWidth;
+            [FieldOffset(14)] public ushort ImageHeight;
+            [FieldOffset(16)] public byte BitPerPixel;
+            [FieldOffset(17)] public byte Descriptor;
+        }
 
-            public TgaData(byte[] image)
+        [StructLayout(LayoutKind.Explicit)]
+        private struct StructAsByteArray
+        {
+            [FieldOffset(0)]
+            private TgaHeader[] HeaderData;
+            [FieldOffset(0)]
+            private byte[] ByteData;
+
+            public StructAsByteArray(int i)
             {
-                this.idFieldLength = image[0];
-                this.colorMapType = image[1];
-                this.imageType = image[2];
-                this.colorMapIndex = image[4] << 8 | image[3];
-                this.colorMapLength = image[6] << 8 | image[5];
-                this.colorMapDepth = image[7];
-                this.imageOriginX = image[9] << 8 | image[8];
-                this.imageOriginY = image[11] << 8 | image[10];
-                this.imageWidth = image[13] << 8 | image[12];
-                this.imageHeight = image[15] << 8 | image[14];
-                this.bitPerPixel = image[16];
-                this.descriptor = image[17];
-                this.colorData = new byte[image.Length - TgaHeaderSize];
-                Array.Copy(image, TgaHeaderSize, this.colorData, 0, this.colorData.Length);
-                // Index color RLE or Full color RLE or Gray RLE
-                if (this.imageType == 9 || this.imageType == 10 || this.imageType == 11)
-                    this.colorData = this.decodeRLE();
+                HeaderData = null;
+                ByteData = new byte[Marshal.SizeOf<TgaHeader>()];
             }
 
-            public int Width
+            public ref TgaHeader Data => ref HeaderData[0];
+            public byte[] Buffer => ByteData;
+        }
+
+        public static Bitmap FromFile(string filename)
+        {
+            using (var stream = File.OpenRead(filename))
             {
-                get { return this.imageWidth; }
+                return FromStream(stream);
+            }
+        }
+        
+        public static Bitmap FromStream(Stream stream)
+        {
+            var header = new StructAsByteArray(0);
+            var headerBuffer = header.Buffer;
+            ref var headerData = ref header.Data;
+            stream.Read(headerBuffer, 0, headerBuffer.Length);
+            if (headerData.ColorMapType != 0 ||
+                headerData.ImageType != 2 && headerData.ImageType != 10 ||
+                headerData.BitPerPixel != 24 && headerData.BitPerPixel != 32)
+            {
+                throw new NotImplementedException();
             }
 
-            public int Height
+            var colorData = new byte[stream.Length - stream.Position];
+            stream.Read(colorData, 0, colorData.Length);
+            var ret = new Bitmap(headerData.ImageWidth, headerData.ImageHeight, PixelFormat.Format32bppArgb);
+
+            int elementCount = headerData.BitPerPixel / 8;
+            if (headerData.ImageType == 9 || headerData.ImageType == 10 || headerData.ImageType == 11)
             {
-                get { return this.imageHeight; }
-            }
-
-            public int GetPixel(int x, int y)
-            {
-                if (colorMapType == 0)
-                {
-                    switch (this.imageType)
-                    {
-                        // Index color
-                        case 1:
-                        case 9:
-                            // not implemented
-                            return 0;
-
-                        // Full color
-                        case 2:
-                        case 10:
-                            int elementCount = this.bitPerPixel / 8;
-                            int dy = ((this.descriptor & 0x20) == 0 ? (this.imageHeight - 1 - y) : y) * (this.imageWidth * elementCount);
-                            int dx = ((this.descriptor & 0x10) == 0 ? x : (this.imageWidth - 1 - x)) * elementCount;
-                            int index = dy + dx;
-
-                            int b = this.colorData[index + 0] & 0xFF;
-                            int g = this.colorData[index + 1] & 0xFF;
-                            int r = this.colorData[index + 2] & 0xFF;
-
-                            if (elementCount == 4) // this.bitPerPixel == 32
-                            {
-                                int a = this.colorData[index + 3] & 0xFF;
-                                return (a << 24) | (r << 16) | (g << 8) | b;
-                            }
-                            else if (elementCount == 3) // this.bitPerPixel == 24
-                            {
-                                return (r << 16) | (g << 8) | b;
-                            }
-                            break;
-
-                        // Gray
-                        case 3:
-                        case 11:
-                            // not implemented
-                            return 0;
-                    }
-                    return 0;
-                }
-                else
-                {
-                    // not implemented
-                    return 0;
-                }
-            }
-
-            protected byte[] decodeRLE()
-            {
-                int elementCount = this.bitPerPixel / 8;
                 byte[] elements = new byte[elementCount];
-                int decodeBufferLength = elementCount * this.imageWidth * this.imageHeight;
+                int decodeBufferLength = elementCount * ret.Width * ret.Height;
                 byte[] decodeBuffer = new byte[decodeBufferLength];
                 int decoded = 0;
                 int offset = 0;
                 while (decoded < decodeBufferLength)
                 {
-                    int packet = this.colorData[offset++] & 0xFF;
+                    int packet = colorData[offset++];
                     if ((packet & 0x80) != 0)
                     {
                         for (int i = 0; i < elementCount; i++)
                         {
-                            elements[i] = this.colorData[offset++];
+                            elements[i] = colorData[offset++];
                         }
                         int count = (packet & 0x7F) + 1;
                         for (int i = 0; i < count; i++)
@@ -139,50 +103,43 @@ namespace MMDRenderer
                         int count = (packet + 1) * elementCount;
                         for (int i = 0; i < count; i++)
                         {
-                            decodeBuffer[decoded++] = this.colorData[offset++];
+                            decodeBuffer[decoded++] = colorData[offset++];
                         }
                     }
                 }
-                return decodeBuffer;
+                colorData = decodeBuffer;
             }
-        }
 
-        public static Bitmap FromFile(string path)
-        {
+            var bitmapData = ret.LockBits(new Rectangle(0, 0, ret.Width, ret.Height),
+                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                for (int y = 0; y < ret.Height; ++y)
                 {
-                    int length = (int)fs.Length;
-                    byte[] buffer = new byte[length];
-                    fs.Read(buffer, 0, length);
-                    return decode(buffer);
+                    var dy = (headerData.Descriptor & 0x20) == 0 ? (ret.Height - 1 - y) : y;
+                    dy *= (ret.Width * elementCount);
+                    for (int x = 0; x < ret.Width; ++x)
+                    {
+                        var dx = (headerData.Descriptor & 0x10) == 0 ? x : (ret.Width - 1 - x);
+                        dx *= elementCount;
+                        var index = dx + dy;
+
+                        int b = colorData[index + 0];
+                        int g = colorData[index + 1];
+                        int r = colorData[index + 2];
+                        int a = 255;
+                        if (elementCount == 4) a = colorData[index + 3];
+                        int color = (a << 24) | (r << 16) | (g << 8) | b;
+
+                        Marshal.WriteInt32(bitmapData.Scan0, bitmapData.Stride * y + 4 * x, color);
+                    }
                 }
             }
-            catch (Exception)
+            finally
             {
-                return null;
+                ret.UnlockBits(bitmapData);
             }
-        }
-
-        public static Bitmap FromBinary(byte[] image)
-        {
-            return decode(image);
-        }
-
-        protected static Bitmap decode(byte[] image)
-        {
-            TgaData tga = new TgaData(image);
-
-            Bitmap bitmap = new Bitmap(tga.Width, tga.Height);
-            for (int y = 0; y < tga.Height; ++y)
-            {
-                for (int x = 0; x < tga.Width; ++x)
-                {
-                    bitmap.SetPixel(x, y, Color.FromArgb(tga.GetPixel(x, y)));
-                }
-            }
-            return bitmap;
+            return ret;
         }
     }
 }
